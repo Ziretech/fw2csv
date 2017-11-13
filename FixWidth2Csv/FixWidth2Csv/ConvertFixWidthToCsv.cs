dc59ace9cc4533c009ab7b70df3f7700bb6b4b8c
jofhod02 <josef.hoglund@umu.se>
2017-11-13 19:53:29 +0100

    Converter handles the most simple case of one column.

---
+++ FixWidth2Csv/FixWidth2Csv/ConvertFixWidthToCsv.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixWidth2Csv
+{
+    public class ConvertFixWidthToCsv
+    {
+        public IWriter Writer { get; set; }
+
+        public void Convert(IReader reader)
+        {
+            var headers = reader.ReadLine(1);
+            var delimiters = reader.ReadLine(1);
+            var rows = reader.ReadLine(1);
+
+            Writer.WriteRow(new[] { headers });
+            Writer.WriteRow(new[] { rows });
+        }
+    }
+}
---
+++ FixWidth2Csv/FixWidth2CsvTest/ConvertFixWidthToCsvSpec.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FixWidth2Csv;
+using NUnit.Framework;
+
+namespace FixWidth2CsvTest
+{
+    [TestFixture]
+    public class ConvertFixWidthToCsvSpec
+    {
+        private WriterMock _writer;
+        private ReaderMock _reader;
+        private ConvertFixWidthToCsv _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _writer = new WriterMock();
+            _reader = new ReaderMock();
+            _converter = new ConvertFixWidthToCsv { Writer = _writer };
+        }
+
+        [Test]
+        public void Converter_converts_simple_text_to_csv()
+        {
+            _reader.AddLine("a");
+            _reader.AddLine("-");
+            _reader.AddLine("b");
+            _converter.Convert(_reader);
+
+            Assert.That(_writer.RowList[0], Is.EquivalentTo(new [] { "a" }));
+            Assert.That(_writer.RowList[1], Is.EquivalentTo(new [] { "b" }));
+        }
+
+        [Test]
+        public void Converter_converts_other_simple_text_to_csv()
+        {
+            _reader.AddLine("namn");
+            _reader.AddLine("----");
+            _reader.AddLine("ola");
+            _converter.Convert(_reader);
+
+            Assert.That(_writer.RowList[0], Is.EquivalentTo(new[] { "namn" }));
+            Assert.That(_writer.RowList[1], Is.EquivalentTo(new[] { "ola" }));
+        }
+    }
+}
